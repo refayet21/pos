d6cf7f03f94dad63909c72623f4c1a0b6994ceea
refayet21 <refayet21@gmail.com>
2024-05-19 06:31:12 +0600

    cart and calculation added

--- lib/presentation/home/controllers/home.controller.dart
+++ lib/presentation/home/controllers/home.controller.dart
@@ -3,10 +3,105 @@ import 'package:get/get.dart';
 import 'package:loyverspos/domain/DBHelper.dart';
 import 'package:loyverspos/model/item_model.dart';
 
-class HomeController extends GetxController {
-  // var allItems = <ItemModel>[].obs;
-  RxList<ItemModel> allItems = RxList<ItemModel>([]);
+// class HomeController extends GetxController {
+//   // var allItems = <ItemModel>[].obs;
+//   RxList<ItemModel> allItems = RxList<ItemModel>([]);
 
+//   final DatabaseHelper databaseHelper = DatabaseHelper();
+//   final count = 0.obs;
+
+//   @override
+//   void onInit() {
+//     super.onInit();
+//     fetchAllItems();
+//   }
+
+//   // void fetchAllItems() async {
+//   //   var items = await databaseHelper.getItems();
+//   //   allItems.value = items;
+//   // }
+//   void fetchAllItems() async {
+//     try {
+//       var items = await databaseHelper.getItems();
+//       allItems.value = items;
+//     } catch (e) {
+//       print('Error fetching items: $e');
+//     }
+//   }
+
+//   var cartItems = <ItemModel>[].obs;
+
+//   double get totalPrice =>
+//       cartItems.fold(0, (previousValue, item) => previousValue + item.price);
+
+//   // void addToCart(ItemModel item) {
+//   //   cartItems.add(item);
+//   // }
+//   bool isProductInCart(ItemModel product) {
+//     return cartItems.any((item) => item.id == product.id);
+//   }
+
+//   void addToCart(ItemModel product) {
+//     bool alreadyInCart = cartItems.any((item) => item.id == product.id);
+
+//     if (!alreadyInCart) {
+//       cartItems.add(product);
+//     } else {
+//       print('Product is already in the cart');
+//     }
+//   }
+
+//   // void removeFromCart(ItemModel item) {
+//   //   cartItems.remove(item);
+//   // }
+
+//   // void increment() => count.value++;
+//   // void decrement() => count.value--;
+
+//   void increaseQuantity(ItemModel product) {
+//     final index = cartItems.indexWhere((item) => item.id == product.id);
+//     if (index != -1) {
+//       cartItems[index].quantity++;
+//     }
+//   }
+
+//   void decreaseQuantity(ItemModel product) {
+//     final index = cartItems.indexWhere((item) => item.id == product.id);
+//     if (index != -1 && cartItems[index].quantity > 1) {
+//       cartItems[index].quantity--;
+//     } else {
+//       cartItems.remove(product);
+//     }
+//   }
+
+//   void updateQuantity(ItemModel product, int newQuantity) {
+//     final index = cartItems.indexWhere((item) => item.id == product.id);
+//     if (index != -1) {
+//       if (newQuantity > 0) {
+//         cartItems[index].quantity = newQuantity;
+//       } else {
+//         cartItems.remove(product);
+//       }
+//     }
+//   }
+
+//   double updateprice(
+//     ItemModel product,
+//   ) {
+//     final index = cartItems.indexWhere((item) => item.id == product.id);
+//     var result;
+//     if (index != -1) {
+//       result = cartItems[index].price * cartItems[index].quantity;
+//       return result;
+//     } else {
+//       return 0.0;
+//     }
+//   }
+// }
+
+class HomeController extends GetxController {
+  RxList<ItemModel> allItems = RxList<ItemModel>([]);
+  var cartItems = <ItemModel>[].obs;
   final DatabaseHelper databaseHelper = DatabaseHelper();
   final count = 0.obs;
 
@@ -17,52 +112,46 @@ class HomeController extends GetxController {
   }
 
   void fetchAllItems() async {
-    var items = await databaseHelper.getItems();
-    allItems.value = items;
+    try {
+      var items = await databaseHelper.getItems();
+      allItems.value = items;
+    } catch (e) {
+      print('Error fetching items: $e');
+    }
   }
 
-  var cartItems = <ItemModel>[].obs;
+  double get totalPrice => cartItems.fold(
+      0, (previousValue, item) => previousValue + item.price * item.quantity);
 
-  double get totalPrice =>
-      cartItems.fold(0, (previousValue, item) => previousValue + item.price);
-
-  // void addToCart(ItemModel item) {
-  //   cartItems.add(item);
-  // }
   bool isProductInCart(ItemModel product) {
     return cartItems.any((item) => item.id == product.id);
   }
 
   void addToCart(ItemModel product) {
-    bool alreadyInCart = cartItems.any((item) => item.id == product.id);
-
-    if (!alreadyInCart) {
+    if (!isProductInCart(product)) {
       cartItems.add(product);
     } else {
       print('Product is already in the cart');
     }
   }
 
-  // void removeFromCart(ItemModel item) {
-  //   cartItems.remove(item);
-  // }
-
-  // void increment() => count.value++;
-  // void decrement() => count.value--;
-
   void increaseQuantity(ItemModel product) {
     final index = cartItems.indexWhere((item) => item.id == product.id);
     if (index != -1) {
       cartItems[index].quantity++;
+      updatePrice();
     }
   }
 
   void decreaseQuantity(ItemModel product) {
     final index = cartItems.indexWhere((item) => item.id == product.id);
-    if (index != -1 && cartItems[index].quantity > 1) {
-      cartItems[index].quantity--;
-    } else {
-      cartItems.remove(product);
+    if (index != -1) {
+      if (cartItems[index].quantity > 1) {
+        cartItems[index].quantity--;
+      } else {
+        cartItems.removeAt(index);
+      }
+      updatePrice();
     }
   }
 
@@ -72,8 +161,14 @@ class HomeController extends GetxController {
       if (newQuantity > 0) {
         cartItems[index].quantity = newQuantity;
       } else {
-        cartItems.remove(product);
+        cartItems.removeAt(index);
       }
+      updatePrice();
     }
+  }
+
+  void updatePrice() {
+    cartItems
+        .refresh(); // This will trigger UI updates for all observers of cartItems
   }
 }
--- lib/presentation/receipts/receipts.screen.dart
+++ lib/presentation/receipts/receipts.screen.dart
@@ -7,6 +7,217 @@ import 'package:loyverspos/widgets/drawer.dart';
 
 import 'controllers/receipts.controller.dart';
 
+// class ReceiptsScreen extends GetView<ReceiptsController> {
+//   ReceiptsScreen({Key? key}) : super(key: key);
+//   final HomeController cartController = Get.put(HomeController());
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Cart'),
+//       ),
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: Obx(
+//               () => ListView.builder(
+//                 itemCount: cartController.cartItems.length,
+//                 itemBuilder: (context, index) {
+//                   TextEditingController _quantityController =
+//                       TextEditingController(
+//                           text: cartController.cartItems[index].quantity
+//                               .toString());
+//                   final item = cartController.cartItems[index];
+//                   return ListTile(
+//                     title: Text(item.name),
+//                     subtitle: Column(
+//                       mainAxisAlignment: MainAxisAlignment.start,
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Text('Price: \$${item.price.toStringAsFixed(2)}'),
+//                         SizedBox(
+//                           // width: 15.w, // Adjust the width as needed
+//                           child: TextFormField(
+//                             // controller: _quantityController,
+//                             // keyboardType: TextInputType.number,
+//                             readOnly: true,
+//                             textAlign: TextAlign.center,
+//                             onChanged: (newValue) {
+//                               // Update the quantity when the user changes the value
+//                               cartController.updateprice(
+//                                 cartController.cartItems[index],
+//                               );
+//                             },
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                     // trailing: Row(
+//                     //   mainAxisSize: MainAxisSize.min,
+//                     //   children: [
+//                     //     IconButton(
+//                     //       icon: Icon(Icons.remove),
+//                     //       onPressed: () {
+//                     //         cartController
+//                     //             .decreaseQuantity(
+//                     //             cartController.cartItems[index]);
+//                     //         // _quantityController.text =
+//                     //         //     cartController
+//                     //         //     .cartItems[index].quantity.toString();
+//                     //       },
+//                     //     ),
+//                     //     Obx(
+//                     //       () => Text('${cartController.count.value}'),
+//                     //     ), // Quantity (can be replaced with actual quantity)
+//                     //     IconButton(
+//                     //       icon: Icon(Icons.add),
+//                     //       onPressed: () {
+//                     //         cartController.increment;
+//                     //       },
+//                     //     ),
+//                     //   ],
+//                     // ),
+
+//                     trailing: Row(
+//                       mainAxisSize: MainAxisSize.min,
+//                       children: [
+//                         IconButton(
+//                           icon: Icon(Icons.remove),
+//                           onPressed: () {
+//                             cartController.decreaseQuantity(
+//                                 cartController.cartItems[index]);
+//                             _quantityController.text = cartController
+//                                 .cartItems[index].quantity
+//                                 .toString();
+//                           },
+//                         ),
+//                         SizedBox(
+//                           width: 15.w, // Adjust the width as needed
+//                           child: TextFormField(
+//                             controller: _quantityController,
+//                             keyboardType: TextInputType.number,
+//                             textAlign: TextAlign.center,
+//                             onChanged: (newValue) {
+//                               // Update the quantity when the user changes the value
+//                               cartController.updateQuantity(
+//                                   cartController.cartItems[index],
+//                                   int.parse(newValue));
+//                             },
+//                           ),
+//                         ),
+//                         IconButton(
+//                           icon: Icon(Icons.add),
+//                           onPressed: () {
+//                             cartController.increaseQuantity(
+//                                 cartController.cartItems[index]);
+//                             _quantityController.text = cartController
+//                                 .cartItems[index].quantity
+//                                 .toString();
+//                           },
+//                         ),
+//                       ],
+//                     ),
+//                   );
+//                 },
+//               ),
+//             ),
+//           ),
+//           // Padding(
+//           //   padding: const EdgeInsets.all(8.0),
+//           //   child: Text(
+//           //     'Total Price: \$${cartController.totalPrice.toStringAsFixed(2)}',
+//           //     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//           //   ),
+//           // ),
+
+//           ElevatedButton(
+//             onPressed: () async {
+//               // Create a list to hold purchaseInfo data for each item
+//               List<String> purchaseInfoList = [];
+
+//               // Add vendor and date information
+//               // String vendorInfo =
+//               //     'Vendor Name: ${selectedVendor?.name ?? "N/A"}';
+//               // String dateInfo = 'Date: ${dateController.text}';
+//               // purchaseInfoList.add('$vendorInfo\n$dateInfo\n');
+
+//               // Add cart items information
+//               for (var index = 0;
+//                   index < cartController.cartItems.length;
+//                   index++) {
+//                 var item = cartController.cartItems[index];
+
+//                 String itemInfo = '';
+//                 itemInfo += 'Product Name: ${item.name ?? "N/A"}\n';
+//                 itemInfo += 'Barcode: ${item.barcode ?? "N/A"}\n';
+//                 itemInfo += 'Price: ${item.price?.toString() ?? "N/A"}\n';
+//                 // itemInfo += 'Stock: ${item.stock?.toString() ?? "N/A"}\n';
+//                 // itemInfo +=
+//                 //     'New Stock: ${(item.stock! + item.quantity).toString()}\n';
+//                 itemInfo += '---\n'; // Separator between items, for clarity
+
+//                 // Add the purchaseInfo data to the list
+//                 purchaseInfoList.add(itemInfo);
+//               }
+
+//               // Show a dialog with the previous purchase information
+//               await showDialog(
+//                 context: context,
+//                 builder: (BuildContext context) {
+//                   return AlertDialog(
+//                     title: Text('Previous Purchase Information'),
+//                     content: SingleChildScrollView(
+//                       child: Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children:
+//                             purchaseInfoList.map((info) => Text(info)).toList(),
+//                       ),
+//                     ),
+//                     actions: <Widget>[
+//                       TextButton(
+//                         onPressed: () async {
+//                           // Continue with the purchase confirmation using updated cart items
+//                           // await cartController.addPurchaseData(
+//                           //   vendorDocId: selectedVendor!.docId,
+//                           //   date: dateController.text,
+//                           //   cartItems: controller.cartItems
+//                           //       .map((item) => item.toJson())
+//                           //       .toList(),
+//                           // );
+
+//                           // Close the dialog
+//                           Navigator.of(context).pop();
+//                         },
+//                         child: Text('Confirm'),
+//                       ),
+//                       TextButton(
+//                         onPressed: () {
+//                           // Close the dialog without saving
+//                           Navigator.of(context).pop();
+//                         },
+//                         child: Text('Cancel'),
+//                       ),
+//                     ],
+//                   );
+//                 },
+//               );
+//             },
+//             child: Text('Confirm Purchase'),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:loyverspos/presentation/home/controllers/home.controller.dart';
+import 'package:loyverspos/widgets/drawer.dart';
+import 'controllers/receipts.controller.dart';
+
 class ReceiptsScreen extends GetView<ReceiptsController> {
   ReceiptsScreen({Key? key}) : super(key: key);
   final HomeController cartController = Get.put(HomeController());
@@ -24,75 +235,64 @@ class ReceiptsScreen extends GetView<ReceiptsController> {
               () => ListView.builder(
                 itemCount: cartController.cartItems.length,
                 itemBuilder: (context, index) {
-                  TextEditingController _quantityController =
-                      TextEditingController(
-                          text: cartController.cartItems[index].quantity
-                              .toString());
                   final item = cartController.cartItems[index];
+                  TextEditingController _quantityController =
+                      TextEditingController(text: item.quantity.toString());
+
                   return ListTile(
                     title: Text(item.name),
-                    subtitle: Text('Price: \$${item.price.toStringAsFixed(2)}'),
-                    // trailing: Row(
-                    //   mainAxisSize: MainAxisSize.min,
-                    //   children: [
-                    //     IconButton(
-                    //       icon: Icon(Icons.remove),
-                    //       onPressed: () {
-                    //         cartController
-                    //             .decreaseQuantity(
-                    //             cartController.cartItems[index]);
-                    //         // _quantityController.text =
-                    //         //     cartController
-                    //         //     .cartItems[index].quantity.toString();
-                    //       },
-                    //     ),
-                    //     Obx(
-                    //       () => Text('${cartController.count.value}'),
-                    //     ), // Quantity (can be replaced with actual quantity)
-                    //     IconButton(
-                    //       icon: Icon(Icons.add),
-                    //       onPressed: () {
-                    //         cartController.increment;
-                    //       },
-                    //     ),
-                    //   ],
-                    // ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                            'Price: \₹ ${item.price.toStringAsFixed(2)} × ${_quantityController.text}'),
+                        // SizedBox(
+                        //   width: 50.w,
+                        //   child: TextFormField(
+                        //     readOnly: true,
+                        //     textAlign: TextAlign.center,
+                        //     controller: _quantityController,
+                        //   ),
+                        // ),
+                        // Obx(() => Text(
+                        //     'Total: \$${(item.price * item.quantity).toStringAsFixed(2)}')),
 
+                        Text(
+                            'Total: \₹ ${(item.price * item.quantity).toStringAsFixed(2)}')
+                      ],
+                    ),
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         IconButton(
                           icon: Icon(Icons.remove),
                           onPressed: () {
-                            cartController.decreaseQuantity(
-                                cartController.cartItems[index]);
-                            _quantityController.text = cartController
-                                .cartItems[index].quantity
-                                .toString();
+                            cartController.decreaseQuantity(item);
+                            _quantityController.text = item.quantity.toString();
                           },
                         ),
                         SizedBox(
-                          width: 15.w, // Adjust the width as needed
+                          width: 50.w,
                           child: TextFormField(
                             controller: _quantityController,
                             keyboardType: TextInputType.number,
                             textAlign: TextAlign.center,
                             onChanged: (newValue) {
-                              // Update the quantity when the user changes the value
-                              cartController.updateQuantity(
-                                  cartController.cartItems[index],
-                                  int.parse(newValue));
+                              try {
+                                int newQuantity = int.parse(newValue);
+                                cartController.updateQuantity(
+                                    item, newQuantity);
+                              } catch (e) {
+                                // handle error if newValue is not a valid integer
+                              }
                             },
                           ),
                         ),
                         IconButton(
                           icon: Icon(Icons.add),
                           onPressed: () {
-                            cartController.increaseQuantity(
-                                cartController.cartItems[index]);
-                            _quantityController.text = cartController
-                                .cartItems[index].quantity
-                                .toString();
+                            cartController.increaseQuantity(item);
+                            _quantityController.text = item.quantity.toString();
                           },
                         ),
                       ],
@@ -102,45 +302,29 @@ class ReceiptsScreen extends GetView<ReceiptsController> {
               ),
             ),
           ),
-          // Padding(
-          //   padding: const EdgeInsets.all(8.0),
-          //   child: Text(
-          //     'Total Price: \$${cartController.totalPrice.toStringAsFixed(2)}',
-          //     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          //   ),
-          // ),
-
+          Obx(() => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Total Price: \₹ ${cartController.totalPrice.toStringAsFixed(2)}',
+                  style:
+                      TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+                ),
+              )),
           ElevatedButton(
             onPressed: () async {
-              // Create a list to hold purchaseInfo data for each item
               List<String> purchaseInfoList = [];
 
-              // Add vendor and date information
-              // String vendorInfo =
-              //     'Vendor Name: ${selectedVendor?.name ?? "N/A"}';
-              // String dateInfo = 'Date: ${dateController.text}';
-              // purchaseInfoList.add('$vendorInfo\n$dateInfo\n');
-
-              // Add cart items information
-              for (var index = 0;
-                  index < cartController.cartItems.length;
-                  index++) {
-                var item = cartController.cartItems[index];
-
-                String itemInfo = '';
-                itemInfo += 'Product Name: ${item.name ?? "N/A"}\n';
-                itemInfo += 'Barcode: ${item.barcode ?? "N/A"}\n';
-                itemInfo += 'Price: ${item.price?.toString() ?? "N/A"}\n';
-                // itemInfo += 'Stock: ${item.stock?.toString() ?? "N/A"}\n';
-                // itemInfo +=
-                //     'New Stock: ${(item.stock! + item.quantity).toString()}\n';
-                itemInfo += '---\n'; // Separator between items, for clarity
-
-                // Add the purchaseInfo data to the list
+              for (var item in cartController.cartItems) {
+                String itemInfo = '''
+                  Product Name: ${item.name}
+                  Barcode: ${item.barcode}
+                  Price: \$${item.price}
+                  Quantity: ${item.quantity}
+                  Total: \$${(item.price * item.quantity).toStringAsFixed(2)}
+                ''';
                 purchaseInfoList.add(itemInfo);
               }
 
-              // Show a dialog with the previous purchase information
               await showDialog(
                 context: context,
                 builder: (BuildContext context) {
@@ -155,24 +339,13 @@ class ReceiptsScreen extends GetView<ReceiptsController> {
                     ),
                     actions: <Widget>[
                       TextButton(
-                        onPressed: () async {
-                          // Continue with the purchase confirmation using updated cart items
-                          // await cartController.addPurchaseData(
-                          //   vendorDocId: selectedVendor!.docId,
-                          //   date: dateController.text,
-                          //   cartItems: controller.cartItems
-                          //       .map((item) => item.toJson())
-                          //       .toList(),
-                          // );
-
-                          // Close the dialog
+                        onPressed: () {
                           Navigator.of(context).pop();
                         },
                         child: Text('Confirm'),
                       ),
                       TextButton(
                         onPressed: () {
-                          // Close the dialog without saving
                           Navigator.of(context).pop();
                         },
                         child: Text('Cancel'),
@@ -183,7 +356,7 @@ class ReceiptsScreen extends GetView<ReceiptsController> {
               );
             },
             child: Text('Confirm Purchase'),
-          )
+          ),
         ],
       ),
     );
