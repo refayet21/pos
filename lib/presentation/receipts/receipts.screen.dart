a7307265a8effc90d0f86105c940dc95834a73c3
refayet21 <refayet21@gmail.com>
2024-05-20 09:22:09 +0600

    print option added

--- lib/presentation/receipts/receipts.screen.dart
+++ lib/presentation/receipts/receipts.screen.dart
@@ -1,3 +1,4 @@
+import 'package:blue_thermal_printer/blue_thermal_printer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -9,6 +10,7 @@ class ReceiptsScreen extends GetView<ReceiptsController> {
   ReceiptsScreen({Key? key}) : super(key: key) {
     Get.put(ReceiptsController());
   }
+  final BlueThermalPrinter bluetooth = BlueThermalPrinter.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -72,7 +74,14 @@ class ReceiptsScreen extends GetView<ReceiptsController> {
                         Icons.print,
                         color: Colors.blue,
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        _printReceipt(
+                            controller.receipts[index]['receiptNo'],
+                            controller.receipts[index]['date'],
+                            convertedList,
+                            controller.receipts[index]['totalPrice']);
+                        // print(convertedList);
+                      },
                     ),
                   ),
                 ),
@@ -82,5 +91,40 @@ class ReceiptsScreen extends GetView<ReceiptsController> {
         },
       ),
     );
+  }
+
+  Future<void> _printReceipt(String receiptNo, String date,
+      List<List<dynamic>> data, String totalPrice) async {
+    // Check if the Bluetooth is connected
+    if ((await bluetooth.isConnected) == true) {
+      bluetooth.printCustom("Receipt No: $receiptNo", 3, 1);
+      bluetooth.printCustom("Date: $date", 1, 1);
+      // Print each item in the receipt data
+      for (var item in data) {
+        bluetooth.printCustom("Product: ${item[0]}", 1,
+            1); // Assuming item[0] is the product name
+        bluetooth.printCustom(
+            "Price: ${item[1]}", 1, 1); // Assuming item[1] is the price
+        bluetooth.printCustom(
+            "Quantity: ${item[2]}", 1, 1); // Assuming item[2] is the quantity
+        bluetooth.printCustom(
+            "Total: ${item[3]}", 1, 1); // Assuming item[3] is the total
+      }
+      bluetooth.printCustom("Total Price: $totalPrice", 1, 1);
+      bluetooth.printNewLine();
+      bluetooth.printNewLine();
+      bluetooth.paperCut();
+    } else {
+      // Connect to the Bluetooth printer
+      List<BluetoothDevice> devices = await bluetooth.getBondedDevices();
+      // Assuming you have one device, otherwise prompt the user to select a device
+      if (devices.isNotEmpty) {
+        await bluetooth.connect(devices.first);
+        _printReceipt(receiptNo, date, data,
+            totalPrice); // Retry printing after connection
+      } else {
+        Get.snackbar("Error", "No Bluetooth devices found");
+      }
+    }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import blue_thermal_printer
 import cloud_firestore
 import firebase_core
 import path_provider_foundation
@@ -12,6 +13,7 @@ import printing
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  BlueThermalPrinterPlugin.register(with: registry.registrar(forPlugin: "BlueThermalPrinterPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
