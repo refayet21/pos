de7cbcd144672655bf89c1b852ec300c344a4758
refayet21 <refayet21@gmail.com>
2024-05-21 20:19:52 +0600

    barcode display properly

--- lib/presentation/home/home.screen.dart
+++ lib/presentation/home/home.screen.dart
@@ -51,52 +51,6 @@ import 'package:loyverspos/widgets/drawer.dart';
 //   }
 // }
 
-class HomeScreen extends GetView<HomeController> {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: AdminDrawer(),
-      appBar: AppBar(title: Text('SALE')),
-      body: Obx(() {
-        // Get the unique groups based on the first two characters
-        final uniqueGroups = _getUniqueGroups(controller.allItems);
-
-        return ListView.builder(
-          itemCount: uniqueGroups.length,
-          itemBuilder: (context, index) {
-            final group = uniqueGroups[index];
-            return ListTile(
-              title: Text(group),
-              onTap: () => _showMatchingBarcodesPage(group),
-            );
-          },
-        );
-      }),
-    );
-  }
-
-  List<String> _getUniqueGroups(List<ItemModel> items) {
-    final Set<String> groups = {};
-    for (var item in items) {
-      if (item.barcode.length >= 2) {
-        // Add the first two characters to the set
-        groups.add(item.barcode.substring(0, 2));
-      }
-    }
-    return groups.toList();
-  }
-
-  void _showMatchingBarcodesPage(String group) {
-    List<ItemModel> matchingItems = controller.allItems
-        .where((item) => item.barcode.startsWith(group))
-        .toList();
-
-    Get.to(() => MatchingBarcodesPage(matchingItems));
-  }
-}
-
 // class HomeScreen extends GetView<HomeController> {
 //   const HomeScreen({Key? key}) : super(key: key);
 
@@ -106,7 +60,7 @@ class HomeScreen extends GetView<HomeController> {
 //       drawer: AdminDrawer(),
 //       appBar: AppBar(title: Text('SALE')),
 //       body: Obx(() {
-//         // Get the unique groups based on the first one or two alphabetic characters
+//         // Get the unique groups based on the first two characters
 //         final uniqueGroups = _getUniqueGroups(controller.allItems);
 
 //         return ListView.builder(
@@ -126,22 +80,12 @@ class HomeScreen extends GetView<HomeController> {
 //   List<String> _getUniqueGroups(List<ItemModel> items) {
 //     final Set<String> groups = {};
 //     for (var item in items) {
-//       String barcode = item.barcode;
-//       if (barcode.isNotEmpty && _isAlphabetic(barcode.substring(0, 1))) {
-//         // Add the first character to the set if it's an alphabetic character
-//         groups.add(barcode.substring(0, 1));
-//       }
-//       if (barcode.length >= 2 && _isAlphabetic(barcode.substring(0, 2))) {
-//         // Add the first two characters to the set if both are alphabetic characters
-//         groups.add(barcode.substring(0, 2));
+//       if (item.barcode.length >= 2) {
+//         // Add the first two characters to the set
+//         groups.add(item.barcode.substring(0, 2));
 //       }
 //     }
 //     return groups.toList();
-//   }
-
-//   bool _isAlphabetic(String str) {
-//     final RegExp alphabetic = RegExp(r'^[a-zA-Z]+$');
-//     return alphabetic.hasMatch(str);
 //   }
 
 //   void _showMatchingBarcodesPage(String group) {
@@ -152,3 +96,126 @@ class HomeScreen extends GetView<HomeController> {
 //     Get.to(() => MatchingBarcodesPage(matchingItems));
 //   }
 // }
+
+// class HomeScreen extends GetView<HomeController> {
+//   const HomeScreen({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       drawer: AdminDrawer(),
+//       appBar: AppBar(title: Text('SALE')),
+//       body: Obx(() {
+//         final uniqueLengths = _getUniqueAlphabetLengths(controller.allItems);
+
+//         return ListView.builder(
+//           itemCount: uniqueLengths.length,
+//           itemBuilder: (context, index) {
+//             final length = uniqueLengths[index];
+//             return ListTile(
+//               title: Text('Alphabet Length: $length'),
+//               onTap: () => _showMatchingBarcodesPage(length),
+//             );
+//           },
+//         );
+//       }),
+//     );
+//   }
+
+//   List<int> _getUniqueAlphabetLengths(List<ItemModel> items) {
+//     final Set<int> lengths = {};
+//     for (var item in items) {
+//       String barcode = item.barcode;
+//       int alphabetLength = _getAlphabetLength(barcode);
+//       if (alphabetLength > 0) {
+//         lengths.add(alphabetLength);
+//       }
+//     }
+//     return lengths.toList()..sort();
+//   }
+
+//   int _getAlphabetLength(String barcode) {
+//     if (barcode.length >= 2 && _isAlphabetic(barcode.substring(0, 2))) {
+//       return 2; // Both characters are alphabetic
+//     } else if (barcode.length >= 1 && _isAlphabetic(barcode.substring(0, 1))) {
+//       return 1; // Only the first character is alphabetic
+//     }
+//     return 0; // No alphabetic characters at the start
+//   }
+
+//   bool _isAlphabetic(String str) {
+//     final RegExp alphabetic = RegExp(r'^[a-zA-Z]+$');
+//     return alphabetic.hasMatch(str);
+//   }
+
+//   void _showMatchingBarcodesPage(int alphabetLength) {
+//     List<ItemModel> matchingItems = controller.allItems
+//         .where((item) => _getAlphabetLength(item.barcode) == alphabetLength)
+//         .toList();
+
+//     Get.to(() => MatchingBarcodesPage(matchingItems));
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends GetView<HomeController> {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: AdminDrawer(),
+      appBar: AppBar(title: Text('SALE')),
+      body: Obx(() {
+        final uniqueAlphabets = _getUniqueAlphabets(controller.allItems);
+
+        return ListView.builder(
+          itemCount: uniqueAlphabets.length,
+          itemBuilder: (context, index) {
+            final alphabet = uniqueAlphabets[index];
+            return ListTile(
+              title: Text('$alphabet'),
+              onTap: () => _showMatchingBarcodesPage(alphabet),
+            );
+          },
+        );
+      }),
+    );
+  }
+
+  List<String> _getUniqueAlphabets(List<ItemModel> items) {
+    final Set<String> alphabets = {};
+    for (var item in items) {
+      String barcode = item.barcode;
+      String alphabet = _getAlphabet(barcode);
+      if (alphabet.isNotEmpty) {
+        alphabets.add(alphabet);
+      }
+    }
+    return alphabets.toList()..sort();
+  }
+
+  String _getAlphabet(String barcode) {
+    if (barcode.length >= 2 && _isAlphabetic(barcode.substring(0, 2))) {
+      return barcode.substring(0, 2); // Both characters are alphabetic
+    } else if (barcode.length >= 1 && _isAlphabetic(barcode.substring(0, 1))) {
+      return barcode.substring(0, 1); // Only the first character is alphabetic
+    }
+    return ''; // No alphabetic characters at the start
+  }
+
+  bool _isAlphabetic(String str) {
+    final RegExp alphabetic = RegExp(r'^[a-zA-Z]+$');
+    return alphabetic.hasMatch(str);
+  }
+
+  void _showMatchingBarcodesPage(String alphabet) {
+    List<ItemModel> matchingItems = controller.allItems
+        .where((item) => _getAlphabet(item.barcode) == alphabet)
+        .toList();
+
+    Get.to(() => MatchingBarcodesPage(matchingItems));
+  }
+}
