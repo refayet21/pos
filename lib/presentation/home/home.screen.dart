7ddc7376509c2bb5320952de8cce89f8934cd4b0
refayet21 <refayet21@gmail.com>
2024-05-21 09:35:41 +0600

    product list sh

--- lib/presentation/home/home.screen.dart
+++ lib/presentation/home/home.screen.dart
@@ -11,6 +11,46 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:loyverspos/widgets/drawer.dart';
 
+// class HomeScreen extends GetView<HomeController> {
+//   const HomeScreen({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       drawer: AdminDrawer(),
+//       appBar: AppBar(title: Text('SALE')),
+//       body: Obx(() {
+//         return ListView.builder(
+//           itemCount: controller.allItems.length,
+//           itemBuilder: (context, index) {
+//             final item = controller.allItems[index];
+//             // Check if the current item's barcode has the same first three characters as the previous item
+//             if (index == 0 ||
+//                 item.barcode.substring(0, 1) !=
+//                     controller.allItems[index - 1].barcode.substring(0, 1)) {
+//               return ListTile(
+//                 title: Text(item.barcode),
+//                 onTap: () => _showMatchingBarcodesPage(item.barcode),
+//               );
+//             } else {
+//               return SizedBox.shrink();
+//             }
+//           },
+//         );
+//       }),
+//     );
+//   }
+
+//   void _showMatchingBarcodesPage(String barcode) {
+//     String firstOneCharacters = barcode.substring(0, 1);
+//     List<ItemModel> matchingItems = controller.allItems
+//         .where((item) => item.barcode.startsWith(firstOneCharacters))
+//         .toList();
+
+//     Get.to(() => MatchingBarcodesPage(matchingItems));
+//   }
+// }
+
 class HomeScreen extends GetView<HomeController> {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -20,31 +60,37 @@ class HomeScreen extends GetView<HomeController> {
       drawer: AdminDrawer(),
       appBar: AppBar(title: Text('SALE')),
       body: Obx(() {
+        // Get the unique groups based on the first two characters
+        final uniqueGroups = _getUniqueGroups(controller.allItems);
+
         return ListView.builder(
-          itemCount: controller.allItems.length,
+          itemCount: uniqueGroups.length,
           itemBuilder: (context, index) {
-            final item = controller.allItems[index];
-            // Check if the current item's barcode has the same first three characters as the previous item
-            if (index == 0 ||
-                item.barcode.substring(0, 3) !=
-                    controller.allItems[index - 1].barcode.substring(0, 3)) {
-              return ListTile(
-                title: Text(item.barcode),
-                onTap: () => _showMatchingBarcodesPage(item.barcode),
-              );
-            } else {
-              return SizedBox.shrink();
-            }
+            final group = uniqueGroups[index];
+            return ListTile(
+              title: Text(group),
+              onTap: () => _showMatchingBarcodesPage(group),
+            );
           },
         );
       }),
     );
   }
 
-  void _showMatchingBarcodesPage(String barcode) {
-    String firstThreeCharacters = barcode.substring(0, 3);
+  List<String> _getUniqueGroups(List<ItemModel> items) {
+    final Set<String> groups = {};
+    for (var item in items) {
+      if (item.barcode.length >= 2) {
+        // Add the first two characters to the set
+        groups.add(item.barcode.substring(0, 2));
+      }
+    }
+    return groups.toList();
+  }
+
+  void _showMatchingBarcodesPage(String group) {
     List<ItemModel> matchingItems = controller.allItems
-        .where((item) => item.barcode.startsWith(firstThreeCharacters))
+        .where((item) => item.barcode.startsWith(group))
         .toList();
 
     Get.to(() => MatchingBarcodesPage(matchingItems));
