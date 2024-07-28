c689a8971f41857146dcb9241359fef8f7197dc7
refayet21 <refayet21@gmail.com>
2024-07-28 10:59:38 +0600

    button and cart item added in add to cart page

--- lib/presentation/home/matchingBarcodesPage .dart
+++ lib/presentation/home/matchingBarcodesPage .dart
@@ -51,118 +51,151 @@ class MatchingBarcodesPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Add to cart'),
       ),
-      body: ListView.builder(
-        itemCount: matchingItems.length,
-        itemBuilder: (context, index) {
-          final item = matchingItems[index];
+      body: Column(
+        children: [
+          Expanded(
+            flex: 10,
+            child: ListView.builder(
+              itemCount: matchingItems.length,
+              itemBuilder: (context, index) {
+                final item = matchingItems[index];
 
-          return Obx(() {
-            bool isInCart = cartController.isProductInCart(item);
-            return ListTile(
-              title: Text(item.name),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Price: \₹${item.price.toStringAsFixed(2)}',
-                  ),
-                  Obx(
-                    () => Text(
-                        'Total: \₹ ${(item.price * item.newQuantity.toInt()).toStringAsFixed(2)}'),
-                  )
-                ],
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.remove,
-                      color: Colors.black,
+                return Obx(() {
+                  bool isInCart = cartController.isProductInCart(item);
+                  return ListTile(
+                    title: Text(item.name),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Price: \₹${item.price.toStringAsFixed(2)}',
+                        ),
+                        Obx(
+                          () => Text(
+                              'Total: \₹ ${(item.price * item.newQuantity.toInt()).toStringAsFixed(2)}'),
+                        )
+                      ],
                     ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.remove,
+                            color: Colors.black,
+                          ),
+                          onPressed: () {
+                            // setState(() {
+                            cartController.decreaseQuantity(item);
+                            // _quantityController.text =
+                            //     item.quantity.toString();
+                            // });
+                          },
+                        ),
+                        SizedBox(
+                          width: 40.w,
+                          // child: TextFormField(
+                          //   // controller: _quantityController,
+                          //   keyboardType: TextInputType.number,
+                          //   textAlign: TextAlign.center,
+                          //   onChanged: (newValue) {
+                          //     try {
+                          //       int newQuantity = int.parse(newValue);
+                          //       cartController.updateQuantity(item, newQuantity);
+                          //     } catch (e) {
+                          //       // handle error if newValue is not a valid integer
+                          //     }
+                          //   },
+                          // ),
+                          child: Obx(
+                            () => SizedBox(
+                              width: 40,
+                              child: Center(
+                                  child: Text('${item.newQuantity.value}')),
+                            ),
+                          ),
+                          // child: Obx(() => Text('${item.newQuantity}')),
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.add,
+                            color: Colors.black,
+                          ),
+                          onPressed: () {
+                            // setState(() {
+                            cartController.increaseQuantity(item);
+                            // _quantityController.text =
+                            //     item.quantity.toString();
+                            // });
+                          },
+                        ),
+                        SizedBox(
+                          width: 20.w,
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            isInCart
+                                ? Icons.shopping_bag
+                                : Icons.shopping_bag_outlined,
+                            color: Colors.green,
+                            size: 40.sp,
+                          ),
+                          // onPressed: () {
+                          //   if (!isInCart) {
+                          //     cartController.addToCart(item);
+                          //   } else {
+                          //     // Optionally, handle removing from the cart
+                          //     // cartController.removeFromCart(item);
+                          //   }
+                          // },
+
+                          onPressed: () {
+                            if (isInCart) {
+                              cartController.removeFromCart(item);
+                            } else {
+                              cartController.addToCart(item);
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                });
+              },
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: EdgeInsets.all(10.w),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
                     onPressed: () {
-                      // setState(() {
-                      cartController.decreaseQuantity(item);
-                      // _quantityController.text =
-                      //     item.quantity.toString();
-                      // });
+                      Get.toNamed(Routes.SALES);
                     },
-                  ),
-                  SizedBox(
-                    width: 40.w,
-                    // child: TextFormField(
-                    //   // controller: _quantityController,
-                    //   keyboardType: TextInputType.number,
-                    //   textAlign: TextAlign.center,
-                    //   onChanged: (newValue) {
-                    //     try {
-                    //       int newQuantity = int.parse(newValue);
-                    //       cartController.updateQuantity(item, newQuantity);
-                    //     } catch (e) {
-                    //       // handle error if newValue is not a valid integer
-                    //     }
-                    //   },
-                    // ),
                     child: Obx(
-                      () => SizedBox(
-                        width: 40,
-                        child: Center(child: Text('${item.newQuantity.value}')),
+                      () => Text(
+                        'CHARGE: \₹ ${cartController.totalPrice.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
                       ),
                     ),
-                    // child: Obx(() => Text('${item.newQuantity}')),
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.add,
-                      color: Colors.black,
-                    ),
-                    onPressed: () {
-                      // setState(() {
-                      cartController.increaseQuantity(item);
-                      // _quantityController.text =
-                      //     item.quantity.toString();
-                      // });
-                    },
                   ),
                   SizedBox(
                     width: 20.w,
                   ),
-                  IconButton(
-                    icon: Icon(
-                      isInCart
-                          ? Icons.shopping_bag
-                          : Icons.shopping_bag_outlined,
-                      color: Colors.green,
-                      size: 40.sp,
-                    ),
-                    // onPressed: () {
-                    //   if (!isInCart) {
-                    //     cartController.addToCart(item);
-                    //   } else {
-                    //     // Optionally, handle removing from the cart
-                    //     // cartController.removeFromCart(item);
-                    //   }
-                    // },
-
-                    onPressed: () {
-                      if (isInCart) {
-                        cartController.removeFromCart(item);
-                      } else {
-                        cartController.addToCart(item);
-                      }
-                    },
-                  ),
+                  Obx(() => Text(
+                        'CART ITEM (${cartController.cartItems.length})',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      )),
                 ],
               ),
-            );
-          });
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          Get.toNamed(Routes.SALES);
-        },
-        label: Obx(() => Text('Cart (${cartController.cartItems.length})')),
+            ),
+          )
+        ],
       ),
     );
   }
